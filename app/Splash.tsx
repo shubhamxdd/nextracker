64fc3e758a754948ab2b2b67f16d85277955e918
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-11-05 14:39:21 +0530

    Some more changes in splash

--- app/Splash.tsx
+++ app/Splash.tsx
@@ -6,18 +6,28 @@ import { BsFillBugFill } from "react-icons/bs";
 
 const Splash = () => {
   const [loading, setIsLoading] = useState(true);
+  const [startAnimation, setStartAnimation] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
-      setIsLoading(false);
+      setStartAnimation(true);
     }, 3000);
+
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
   }, []);
 
   return loading ? (
     <div
-      className={`z-50 absolute w-full h-full bg-zinc-200 flex justify-center items-center`}
+      className={`z-50 absolute w-full h-full bg-zinc-200 flex justify-center items-center transition-opacity duration-2000 ${
+        startAnimation ? "opacity-0" : "opacity-100"
+      }`}
     >
-      <BsFillBugFill size={120} className="animate-pulse" />
+      <BsFillBugFill
+        size={120}
+        className={`animate-pulse ${startAnimation ? "animate-scaleUp" : ""}`}
+      />
       {/* <div className="loader"></div> */}
     </div>
   ) : null;
