e19a2f97cb319af918d5c8afafdee1b20e97903c
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-11-05 19:50:13 +0530

    Update link styles , navbar underline and refactor

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,25 +1,49 @@
+"use client";
+
 import Link from "next/link";
 import { FaBug } from "react-icons/fa";
 import Logo from "./Logo";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <nav className="flex gap-6 mb-5 items-center border-b px-16 max-sm:px-3 max-md:px-4 max-lg:px-10 h-14 justify-between">
       <Logo logo={FaBug} />
       <ul className="flex gap-5">
         <li>
-          <Link href="/" className="group transition duration-300 link_styles">
+          <Link
+            href="/"
+            className={`link_styles group ${
+              pathname === "/" ? "text-neutral-800" : "text-neutral-500"
+            }`}
+          >
             Dashboard
-            <span className="block focus:block active:block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 rounded bg-zinc-700"></span>
+            <span
+              className={`underline_span  ${
+                pathname === "/"
+                  ? "max-w-full"
+                  : "max-w-0 group-hover:max-w-full"
+              }`}
+            ></span>
           </Link>
         </li>
         <li>
           <Link
             href="/issues"
-            className="group transition duration-300 link_styles"
+            className={`link_styles group ${
+              pathname === "/issues" ? "text-neutral-800" : "text-neutral-500"
+            }`}
           >
             Issues
-            <span className="block focus:block active:block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 rounded bg-zinc-700"></span>
+            <span
+              className={`underline_span  ${
+                pathname === "/issues"
+                  ? "max-w-full"
+                  : "max-w-0 group-hover:max-w-full"
+              }`}
+            ></span>
           </Link>
         </li>
       </ul>
