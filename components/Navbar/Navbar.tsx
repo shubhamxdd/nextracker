5ab89e1a81ea9e1de336a68fbb9bd2c7e9c807d1
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-11-05 20:12:37 +0530

    Refactor Navbar links , use array mapping and use classnames package

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,48 +4,46 @@ import Link from "next/link";
 import { FaBug } from "react-icons/fa";
 import Logo from "./Logo";
 import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const Navbar = () => {
+  const linksArray = [
+    {
+      href: "/",
+      text: "Dashboard",
+    },
+    {
+      href: "/issues",
+      text: "Issues",
+    },
+  ];
   const pathname = usePathname();
   console.log(pathname);
   return (
     <nav className="flex gap-6 mb-5 items-center border-b px-16 max-sm:px-3 max-md:px-4 max-lg:px-10 h-14 justify-between">
       <Logo logo={FaBug} />
       <ul className="flex gap-5">
-        <li>
-          <Link
-            href="/"
-            className={`link_styles group ${
-              pathname === "/" ? "text-neutral-800" : "text-neutral-500"
-            }`}
-          >
-            Dashboard
-            <span
-              className={`underline_span  ${
-                pathname === "/"
-                  ? "max-w-full"
-                  : "max-w-0 group-hover:max-w-full"
-              }`}
-            ></span>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/issues"
-            className={`link_styles group ${
-              pathname === "/issues" ? "text-neutral-800" : "text-neutral-500"
-            }`}
-          >
-            Issues
-            <span
-              className={`underline_span  ${
-                pathname === "/issues"
-                  ? "max-w-full"
-                  : "max-w-0 group-hover:max-w-full"
-              }`}
-            ></span>
-          </Link>
-        </li>
+        {linksArray.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classNames({
+                "link_styles group": true,
+                "text-neutral-900": pathname === link.href,
+                "text-neutral-500": pathname !== link.href,
+              })}
+            >
+              {link.text}
+              <span
+                className={classNames({
+                  underline_span: true,
+                  "max-w-full": pathname === link.href,
+                  "max-w-0 group-hover:max-w-full": pathname !== link.href,
+                })}
+              ></span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
