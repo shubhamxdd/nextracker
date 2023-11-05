d7dc8a3e91d98f547ec6381c6586cf6a906875cf
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-11-05 14:21:03 +0530

    create splash

--- app/Splash.tsx
+++ app/Splash.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import "./globals.css"; // Import the CSS file
+import "./globals.css";
 
 const Splash = () => {
   const [loading, setIsLoading] = useState(true);
@@ -13,8 +13,10 @@ const Splash = () => {
   }, []);
 
   return loading ? (
-    <div className={`z-50 absolute w-full h-full text-4xl bg-yellow-400 flex justify-center items-center `}>
-      <div className="animate-bounce">LOADING</div>
+    <div
+      className={`z-50 absolute w-full h-full bg-zinc-200 flex justify-center items-center`}
+    >
+      <div className="loader"></div>
     </div>
   ) : null;
 };
