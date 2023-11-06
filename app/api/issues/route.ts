1c8c365fcf42dc700efb38b1ce2075058ec35edd
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-11-06 13:38:25 +0530

    Add loader when md editor is loading and error handling

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,11 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
 import prisma from "@/prisma/client";
-
-const createIssueSchema = z.object({
-  title: z.string().min(1).max(50),
-  description: z.string().min(1),
-});
+import { createIssueSchema } from "@/zodSchema/createIssueSchema";
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
@@ -13,7 +8,7 @@ export async function POST(request: NextRequest) {
   const { title, description } = body;
 
   if (!valid.success)
-    return NextResponse.json(valid.error.errors, { status: 400 });
+    return NextResponse.json(valid.error.format(), { status: 400 });
 
   const issue = await prisma.issue.create({
     data: { title, description },
--- components/issue/NewIssueForm.tsx
+++ components/issue/NewIssueForm.tsx
@@ -1,46 +1,64 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
-// import SimpleMde from "react-simplemde-editor";
+import { Button, Text, TextField } from "@radix-ui/themes";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import toast from "react-hot-toast";
 import dynamic from "next/dynamic";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/zodSchema/createIssueSchema";
+import { z } from "zod";
+import { BsFillBugFill } from "react-icons/bs";
+// import SimpleMde from "react-simplemde-editor";
 
-interface FormShape {
-  title: string;
-  description: string;
-}
+const SimpleMde = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+  loading: () => (
+    <div className="flex justify-center items-center">
+      <BsFillBugFill size={80} className="animate-pulse my-20 mx-8" />
+      <Text size={"5"}>loading markdown editor</Text>
+    </div>
+  ),
+});
+
+type FormShape = z.infer<typeof createIssueSchema>;
 
 const NewIssueForm = () => {
-  const { register, control, handleSubmit } = useForm<FormShape>();
-  const router = useRouter();
-
-  const SimpleMde = dynamic(() => import("react-simplemde-editor"), {
-    ssr: false,
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormShape>({
+    resolver: zodResolver(createIssueSchema),
   });
+  const router = useRouter();
 
   return (
     <form
       onSubmit={handleSubmit(async (data) => {
-        await axios
-          .post("/api/issues", data)
-          .then(() => {
-            toast.success("Issue created successfully");
-            router.push("/issues");
-          })
-          .catch((error) => {
-            console.log(error);
-            toast.error("Error creating issue");
-          });
+        try {
+          await axios.post("/api/issues", data);
+          toast.success("Issue created successfully");
+          router.push("/issues");
+        } catch (error) {
+          console.log(error);
+
+          toast.error("Error creating issue");
+        }
       })}
-      className="max-w-xl space-y-4"
+      className="max-w-xl space-y-2"
     >
       <TextField.Root>
         <TextField.Input placeholder="Enter title" {...register("title")} />
       </TextField.Root>
+      {errors.title && (
+        <Text color="red" as="p">
+          {errors.title.message}
+        </Text>
+      )}
       <Controller
         name="description"
         render={({ field }) => (
@@ -48,6 +66,11 @@ const NewIssueForm = () => {
         )}
         control={control}
       />
+      {errors.description && (
+        <Text color="red" as="p">
+          {errors.description.message}
+        </Text>
+      )}
       <Button>Create Issue</Button>
     </form>
   );
