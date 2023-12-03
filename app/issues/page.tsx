1b51738287407c6e6893c63bbe2c98d5cc034e70
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-12-03 19:38:22 +0530

    make issues page dynamic

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,4 +1,4 @@
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 import StatusBadge from "@/components/StatusBadge";
 import prisma from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
