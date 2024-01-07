d6b326a58d65040d303d33f85612588338214033
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-01-07 15:37:06 +0530

    update api to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
-import { issueSchema } from "@/zodSchema/createIssueSchema";
+import { patchIssueSchema } from "@/zodSchema/createIssueSchema";
 import { getServerSession } from "next-auth";
 import authOption from "@/app/auth/authOptions";
 
@@ -14,9 +14,18 @@ export async function PATCH(
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
 
   const body = await request.json();
-  const valid = issueSchema.safeParse(body);
+  const { title, description, assignedToUserID } = body;
+  const valid = patchIssueSchema.safeParse(body);
   if (!valid.success)
     return NextResponse.json(valid.error.format(), { status: 400 });
+
+  if (assignedToUserID) {
+    const user = await prisma.user.findUnique({
+      where: { id: assignedToUserID },
+    });
+    if (!user)
+      return NextResponse.json({ message: "User not found" }, { status: 400 });
+  }
 
   const issue = await prisma.issue.findUnique({
     where: {
@@ -29,7 +38,11 @@ export async function PATCH(
 
   const updatedIssue = await prisma.issue.update({
     where: { id: params.params.id },
-    data: { title: body.title, description: body.description },
+    data: {
+      title,
+      description,
+      assignedToUserID,
+    },
   });
 
   return NextResponse.json(updatedIssue, {
--- zodSchema/createIssueSchema.ts
+++ zodSchema/createIssueSchema.ts
@@ -4,3 +4,13 @@ export const issueSchema = z.object({
   title: z.string().min(1, "Title is required").max(50, "Title is too long"),
   description: z.string().min(1, "Description is required"),
 });
+
+export const patchIssueSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(50, "Title is too long")
+    .optional(),
+  description: z.string().min(1, "Description is required").optional(),
+  assignedToUserId: z.string().min(1, "id is required").optional().nullable(),
+});
