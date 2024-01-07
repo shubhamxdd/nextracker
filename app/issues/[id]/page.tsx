c3578c5e3c773a7d212478e5a91c8fc6542a3af3
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-01-07 14:19:15 +0530

    hide edit and delete buttons when user is not logged in

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,12 +4,15 @@ import { notFound } from "next/navigation";
 import EditIssueBtn from "./EditIssueBtn";
 import IssueDetailsPage from "./IssueDetailsPage";
 import DeleteButton from "@/components/DeleteButton";
+import { getServerSession } from "next-auth";
+import authOption from "@/app/auth/authOptions";
 
 interface IssuePageProps {
   params: { id: string };
 }
 
 const IssuePage = async ({ params }: IssuePageProps) => {
+  const session = await getServerSession(authOption);
   const issue = await prisma.issue.findUnique({
     where: {
       id: params.id,
@@ -22,9 +25,13 @@ const IssuePage = async ({ params }: IssuePageProps) => {
         <IssueDetailsPage issue={issue} />
       </Box>
       <Box>
-        <Flex direction="column" gap="4" >
-          <EditIssueBtn id={issue.id} />
-          <DeleteButton id={issue.id} />
+        <Flex direction="column" gap="4">
+          {session && (
+            <>
+              <EditIssueBtn id={issue.id} />
+              <DeleteButton id={issue.id} />
+            </>
+          )}
         </Flex>
       </Box>
     </Grid>
