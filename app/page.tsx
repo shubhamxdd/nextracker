b7b431575cd1b0019981e01de4e3b71458ecd585
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-01-09 15:31:41 +0530

    Build component to show latest issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import LatestIssue from "./LatestIssue";
+
 export default function Home() {
-  return <h1 className="text-2xl font-semibold text-center">Hello duniya!</h1>;
+  return <LatestIssue />;
 }
