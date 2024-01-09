e045c41a3aea407d46debf98ea47170f7fa979d9
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2024-01-09 16:53:05 +0530

    Add issue summary and chart to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,28 @@
+import { Flex, Grid } from "@radix-ui/themes";
+import IssueSummary from "./IssueSummary";
 import LatestIssue from "./LatestIssue";
+import prisma from "@/prisma/client";
+import IssueChart from "./IssueChart";
 
-export default function Home() {
-  return <LatestIssue />;
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  return (
+    <>
+      <Grid columns={{ initial: "1", md: "2" }} gap="5" pb="8">
+        <Flex direction="column" gap="5">
+          <IssueSummary closed={closed} inProgress={inProgress} open={open} />
+          <IssueChart closed={closed} inProgress={inProgress} open={open} />
+        </Flex>
+        <LatestIssue />
+      </Grid>
+    </>
+  );
 }
